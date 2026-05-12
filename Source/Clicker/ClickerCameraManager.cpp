685eb45ba3d03bf7f4232eb717dfd832a9cbdf48
J0Min <rudals489@gmail.com>
2026-05-12 18:51:58 +0900

    init

---
+++ Source/Clicker/ClickerCameraManager.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "ClickerCameraManager.h"
+
+AClickerCameraManager::AClickerCameraManager()
+{
+	// set the min/max pitch
+	ViewPitchMin = -70.0f;
+	ViewPitchMax = 80.0f;
+}
