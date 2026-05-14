49ef9e2f833e5d1acbe7b8c8ed32d89728fc2c06
J0Min <rudals489@gmail.com>
2026-05-14 18:31:47 +0900

    팝업 UI의 변수 변경가능토록 토대cpp생성

--- Source/Clicker/Private/PopUI.cpp
+++ Source/Clicker/Private/PopUI.cpp
@@ -1,4 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Public/PopUI.h"
+#include "Components/TextBlock.h"
+
+void UPopUI::SetPopupValue(int64 Value)
+{
+    if (ScrollText)
+    {
+        FString FormattedString = FString::Printf(TEXT("+%lld"), Value);
+        ScrollText->SetText(FText::FromString(FormattedString));
+    }
+}
--- Source/Clicker/Public/PopUI.h
+++ Source/Clicker/Public/PopUI.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
 #include "PopUI.generated.h"
 
 /**
@@ -13,4 +14,14 @@ UCLASS()
 class CLICKER_API UPopUI : public UUserWidget
 {
 	GENERATED_BODY()
+
+protected:
+	/** Text block to display the popup value, bound to the widget in Blueprints */
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget), Category = "UI")
+	class UTextBlock* ScrollText;
+
+public:
+	/** Sets the value of the popup text with a "+" prefix */
+	UFUNCTION(BlueprintCallable, Category = "UI")
+	void SetPopupValue(int64 Value);
 };
